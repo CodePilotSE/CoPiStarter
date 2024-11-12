fbbea20567a1e778efc5018b2318e889b3d039f9
FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>
2024-11-12 09:35:44 +0100

    changed the way blocks are enqueued
    is now an allow list instead of a disallow list
    
    Signed-off-by: FilipKumlin <95422069+FilipKumlin@users.noreply.github.com>

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,73 +1,62 @@
-wp.domReady( () => {
+wp.domReady(() => {
+  // Get all registered block types
+  const allBlocks = wp.blocks.getBlockTypes();
 
-	wp.blocks.unregisterBlockType( 'core/archives' );
-	wp.blocks.unregisterBlockType( 'core/avatar' );
-	wp.blocks.unregisterBlockType( 'core/calendar' );
-	wp.blocks.unregisterBlockType( 'core/comment-author-name' );
-	wp.blocks.unregisterBlockType( 'core/comment-content' );
-	wp.blocks.unregisterBlockType( 'core/comment-date' );
-	wp.blocks.unregisterBlockType( 'core/comment-edit-link' );
-	wp.blocks.unregisterBlockType( 'core/comment-reply-link' );
-	wp.blocks.unregisterBlockType( 'core/comment-template' );
-	wp.blocks.unregisterBlockType( 'core/comments' );
-	wp.blocks.unregisterBlockType( 'core/comments-pagination' );
-	wp.blocks.unregisterBlockType( 'core/comments-pagination-next' );
-	wp.blocks.unregisterBlockType( 'core/comments-pagination-numbers' );
-	wp.blocks.unregisterBlockType( 'core/comments-pagination-previous' );
-	wp.blocks.unregisterBlockType( 'core/comments-query-loop' );
-	wp.blocks.unregisterBlockType( 'core/comments-title' );
-	wp.blocks.unregisterBlockType( 'core/home-link' );
-	wp.blocks.unregisterBlockType( 'core/latest-comments' );
-	wp.blocks.unregisterBlockType( 'core/latest-posts' );
-	wp.blocks.unregisterBlockType( 'core/legacy-widget' );
-	wp.blocks.unregisterBlockType( 'core/loginout' );
-	wp.blocks.unregisterBlockType( 'core/navigation' );
-	wp.blocks.unregisterBlockType( 'core/navigation-link' );
-	wp.blocks.unregisterBlockType( 'core/navigation-submenu' );
-	wp.blocks.unregisterBlockType( 'core/post-author' );
-	wp.blocks.unregisterBlockType( 'core/post-author-name' );
-	wp.blocks.unregisterBlockType( 'core/post-author-biography' );
-	wp.blocks.unregisterBlockType( 'core/post-comments' );
-	wp.blocks.unregisterBlockType( 'core/post-comments-form' );
-	wp.blocks.unregisterBlockType( 'core/post-content' );
-	wp.blocks.unregisterBlockType( 'core/post-date' );
-	wp.blocks.unregisterBlockType( 'core/post-excerpt' );
-	wp.blocks.unregisterBlockType( 'core/post-featured-image' );
-	wp.blocks.unregisterBlockType( 'core/post-navigation-link' );
-	wp.blocks.unregisterBlockType( 'core/post-template' );
-	wp.blocks.unregisterBlockType( 'core/post-terms' );
-	wp.blocks.unregisterBlockType( 'core/post-title' );
-	wp.blocks.unregisterBlockType( 'core/query' );
-	wp.blocks.unregisterBlockType( 'core/query-no-results' );
-	wp.blocks.unregisterBlockType( 'core/query-pagination' );
-	wp.blocks.unregisterBlockType( 'core/query-pagination-next' );
-	wp.blocks.unregisterBlockType( 'core/query-pagination-numbers' );
-	wp.blocks.unregisterBlockType( 'core/query-pagination-previous' );
-	wp.blocks.unregisterBlockType( 'core/query-title' );
-	wp.blocks.unregisterBlockType( 'core/read-more' );
-	wp.blocks.unregisterBlockType( 'core/site-logo' );
-	wp.blocks.unregisterBlockType( 'core/site-tagline' );
-	wp.blocks.unregisterBlockType( 'core/site-title' );
-	wp.blocks.unregisterBlockType( 'core/social-link' );
-	wp.blocks.unregisterBlockType( 'core/social-links' );
-	wp.blocks.unregisterBlockType( 'core/tag-cloud' );
-	wp.blocks.unregisterBlockType( 'core/term-description' );
+  // Define the allowed blocks
+  const allowedBlocks = [
+      'core/paragraph',
+      'core/image',
+      'core/heading',
+      'core/list',
+      'core/list-item',
+      'core/button',
+      'core/buttons',
+      'core/separator',
+      'core/quote',
+      'core/image',
+      'core/gallery',
+      'core/spacer',
+      'core/featured-image',
+      'core/post-featured-image',
+      'core/post-terms',
+      'core/post-date',
+      'core/post-title'
+
+  ];
+
+  // Define the blocks that you manually want to disallow
+  const disallowedBlocks = [
+    
+  ];
+
+  // Unregister blocks
+  allBlocks.forEach((block) => {
+    var blockName = block.name;
+    if(!allowedBlocks.includes(blockName) && blockName.substring(0, 5) === 'core/'){
+      wp.blocks.unregisterBlockType(blockName);
+    }
+    if(disallowedBlocks.includes(blockName)){
+      wp.blocks.unregisterBlockType(blockName);
+    }
+  });
 
 
-	wp.blocks.unregisterBlockStyle(
-		'core/button',
-		[ 'squared', 'fill' ]
-	);
+  const blockStylesToUnregister = {
+    'core/button': ['squared', 'fill'],
+    'core/separator': ['default', 'wide', 'dots'],
+    'core/quote': ['default', 'large', 'plain']
+  };
 
-
-	wp.blocks.unregisterBlockStyle(
-		'core/separator',
-		[ 'default', 'wide', 'dots' ]
-	);
-
-	wp.blocks.unregisterBlockStyle(
-		'core/quote',
-		[ 'default', 'large', 'plain' ]
-	);
+  Object.keys(blockStylesToUnregister).forEach((block) => {
+      blockStylesToUnregister[block].forEach((style) => {
+          wp.blocks.unregisterBlockStyle(block, style);
+      });
+  });
+  // Trigger change event for ACF fields
+  setTimeout(() => {
+    document.querySelectorAll('.acf-block-has-validation-error [data-name]').forEach(field => {
+      field.dispatchEvent(new Event('change'));
+    });
+  }, 1000); // Adjust timeout as needed to ensure fields are loaded
 
 } );
