386083eb0a4c277717d2cc0b67afc24d08cefead
Simon Wimell <simon@codepilot.se>
2025-09-24 16:18:05 +0200

    Cleaner class name

--- blocks/block-demo/render.php
+++ blocks/block-demo/render.php
@@ -10,7 +10,7 @@
 
 use CoPiStarter\Blocks\Block_Demo;
 $styles = block_gradient_style_tag( $block );
-$classes = cs_block_classes( $block , ['wp-block-cwp-block-demo']);
+$classes = cs_block_classes( $block , ['cwp-block-demo']);
 echo '<section '. $classes . ' '. $styles . '>';
   echo Block_Demo\block_content($block);  
 echo '</section>';
