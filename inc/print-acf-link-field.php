0997ae5fb2da22221071b64a3f5c2b2812a8c69f
Filip <95422069+FilipKumlin@users.noreply.github.com>
2026-05-21 10:18:50 +0200

    removed wrapper element and added fallback text param

--- inc/print-acf-link-field.php
+++ inc/print-acf-link-field.php
@@ -4,10 +4,10 @@
  *
  * @param string $field The field name or field key.
  * @param string $post_id The ID of the post/ page where the field is located.
- * @param string $param_title_value The text value to display. Will override the field title if set.
- * @param string $text_wrapper_element The text wrapper element.
+ * @param string $link_text The text to display for the link. Will override the field title if set.
+ * @param string $fallback_text The text to display if the field is empty.
  * @return void
- * @example cs_print_acf_link_field( 'field_69d5144cc6ccf', get_the_ID(), 'This is a custom title', 'span' );
+ * @example cs_print_acf_link_field( 'field_69d5144cc6ccf', get_the_ID(), 'This is a custom title' , 'Läs mer' );
  */
 function cs_print_acf_link_field( $field, $post_id = false, $link_text = '', $fallback_text = 'Läs mer') {
   if ( empty( $field ) ) return;
@@ -22,20 +22,25 @@ function cs_print_acf_link_field( $field, $post_id = false, $link_text = '', $fa
   $field_value = $field_object['value'];
   $return_format = $field_object['return_format'] ?? '';
   $link = '';
-  $title = '';
   
   if ( $return_format === 'array' ) :
     // If the field is an array, check if the url is set and if not, return
     if ( empty( $field_value['url'] ) ) return;
+
     $link = $field_value['url'];
-    $acf_field_title_value = $field_value['title'];
+    if ( empty( $link_text ) ) :
+      $link_text = !empty( $field_value['title'] ) ? $field_value['title'] : $fallback_text;
+    endif;
   else :
     $link = $field_value;
-    $acf_field_title_value = $field_value;
+    if ( empty( $link_text ) ) :
+      $link_text = $fallback_text;
+    endif;
   endif;
   
-  empty( $param_title_value ) ? $title_value = $acf_field_title_value : $title_value = $param_title_value;
+  if ( empty( $link_text ) ) :
+    $link_text = $fallback_text;
+  endif;
 
-  
-  printf( '<a href="%1$s"> %2$s%3$s%4$s</a>', $link, $wrapper_open, $title_value, $wrapper_close );
+  printf( '<a href="%1$s"> %2$s</a>', $link, $link_text );
 }
